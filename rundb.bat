0cc56ea23cbf452d9b2f64a9c2af9f8ba7525aa3
by200 <ben.yuan@outlook.com>
2013-11-28 14:16:26 -0800

    used path

--- rundb.bat
+++ rundb.bat
@@ -1,5 +1,6 @@
-REM SET DBPATH=C:\Users\ben\Downloads\mongodb-win32-i386-2.4.8\mongodb-win32-i386-2.4.8\data\db
+REM SET DBPATH=C:\Users\ben\Downloads\mongodb-win32-i386-2.4.8\mongodb-win32-i386-2.4.8\
 REM 
 REM start MongoDB Server under path \data\db
+SET PATH=%PATH%;C:\Users\ben\Downloads\mongodb-win32-i386-2.4.8\mongodb-win32-i386-2.4.8\bin
 
-bin\mongod --dbpath \data\db 
+mongod --dbpath \data\db 
